f70f89177b2b36d7d0572053e0bcd3ea6173c087
mich <lagranherob@gmail.com>
2021-06-30 13:55:04 +0200

    Done entities. TODO Repo

---
+++ src/main/java/com/example/instazoo/entity/Post.java
@@ -0,0 +1,39 @@
+package com.example.instazoo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private Set<String> likedUsers = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @OneToMany(cascade = CascadeType.REFRESH)
+    private List<Comment> comments = new ArrayList<>();
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdTime = LocalDateTime.now();
+    }
+
+}
