3d7a23f429ee4591fefaeb3101fc85c5b832f130
mich <lagranherob@gmail.com>
2021-06-30 14:40:43 +0200

    Done repo layer. TODO Security

---
+++ src/main/java/com/example/instazoo/repository/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.example.instazoo.repository;
+
+import com.example.instazoo.entity.Comment;
+import com.example.instazoo.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+}
---
+++ src/main/java/com/example/instazoo/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package com.example.instazoo.repository;
+
+import com.example.instazoo.entity.Post;
+import com.example.instazoo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+
+}
